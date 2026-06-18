8c50489b4662f0d6a088a5b01be0e85689439187
sk1218180-cell <sk1218180@gmail.com>
2026-06-19 02:01:03 +0530

    add dashboard feature with overview endpoint and related routes and Backend is completed over here!......

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import userRouter from './routes/userRoute.js';
 import incomeRouter from './routes/incomeRoute.js';
 import expenseModel from './models/expenseModel.js';
 import expenseRouter from './routes/expenseRoute.js';
+import dashboardRouter from './routes/dashboardRoute.js';
 
 
 
@@ -26,7 +27,8 @@ connectDB();
 // ROUTES
 app.use("/api/user", userRouter);
 app.use("/api/income", incomeRouter);
-app.use("/api/expense", expenseRouter)
+app.use("/api/expense", expenseRouter);
+app.use("/api/dashboard", dashboardRouter)
 
 app.get('/', (req, res) => {
   res.send("API is running...");
